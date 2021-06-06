8a3e086157b50f3ac175e0b129b1e9a86dbff7e0
Vamsi Krishna <vamsikrishnaa.b88@gmail.com>
2021-06-06 21:29:38 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ console.log(`Port is ${process.env.PORT}`);
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.send( "Hello world changed!" );
 } );
 
 // start the Express server
